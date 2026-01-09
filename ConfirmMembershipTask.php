a4bcaaf0cee4201c83f97d5b557c1a53ac87b258
majaroenne <majr@kb.dk>
2026-01-09 15:24:26 +0100

    OMP-2_gdpr_module
    Make confirm membership work on OMP

--- ConfirmMembershipTask.php
+++ ConfirmMembershipTask.php
@@ -14,9 +14,10 @@ namespace APP\plugins\generic\confirmMembership;
  * @copydoc ScheduledTask::executeActions()
  */
 
+use APP\core\Services;
 use APP\plugins\generic\confirmMembership\ConfirmMembershipPlugin;
 use Illuminate\Support\Facades\Mail;
-use PKP\core\PKPApplication;
+use PKP\locale\AppLocale;
 use PKP\mail\Mailable;
 use PKP\scheduledTask\ScheduledTask;
 use PKP\user\Repository;
@@ -45,22 +46,25 @@ class ConfirmMembershipTask extends ScheduledTask {
         $roleIds = explode(',', $pluginSettings->getSetting(CONTEXT_SITE, 'roleids'));
         $mergesUser = $pluginSettings->getSetting(CONTEXT_SITE, 'mergeusername');
         $mergesUserId = $userDao->getByUsername($mergesUser)->getId();
+        $jobsAutoDeleteAge = $pluginSettings->getSetting(CONTEXT_SITE, 'jobsautodelete');
         $timestamp = new \DateTime(Core::getCurrentDate());
-        $timestamp->modify('-' . 2 . ' minutes');
+        $timestamp->modify('-' . $daysmerged . ' day');
         $paras = [Core::getCurrentDate(), $mergesUserId, $maxusers];
-       // $result = DB::select("select user_id from users  WHERE date_last_login < DATE(?) - interval ' $daysSendMail days' and disabled = 0
-       //and user_id != ? order by RANDOM() LIMIT ? ",
-       // $paras);
+        $result = DB::select("select user_id from users  WHERE date_last_login < DATE(?) - interval ' $daysSendMail days' and disabled = 0
+        and user_id != ? order by RANDOM() LIMIT ? ",
+        $paras);
+        $context = Services::get('context')->get(1);
+        $senderMail = $context->getData('contactEmail');
 
-        $result = DB::select("select user_id from users where user_id = 11");
         foreach ($result as $userId) {
 
             $user = $userDao->get($userId->user_id);// It is not time to merge the user.
             if (!is_null($this->getUserSettings($user->getId(), ConfirmMembershipPlugin::SETTING_MEMBERSHIP_MAIL_SEND)) && $timestamp < new DateTime($this->getUserSettings($user->getId(), ConfirmMembershipPlugin::SETTING_MEMBERSHIP_MAIL_SEND))
                 || $this->getUserSettings($user->getId(), ConfirmMembershipPlugin::SETTING_CAN_NOT_DELETE)) {
                 continue;
+
             } else if (!is_null($this->getUserSettings($user->getId(), ConfirmMembershipPlugin::SETTING_MEMBERSHIP_MAIL_SEND)) && $timestamp > new DateTime($this->getUserSettings($user->getId(), ConfirmMembershipPlugin::SETTING_MEMBERSHIP_MAIL_SEND))) {
-                $this->mergeUsers($userDao, $pressDao, $roleIds, $user, $mergesUserId);
+                $this->mergeUsers($userDao, $pressDao, $roleIds, $user, $mergesUserId, $jobsAutoDeleteAge);
                 continue;
             }
 
@@ -81,11 +85,11 @@ class ConfirmMembershipTask extends ScheduledTask {
             }
             $fullName = $user->getFullName() ? $user->getFullName() : $user->getGivenName('en') . ' ' . $user->getFamilyName('en');
             if (!empty($memberPresses)) {
-                error_log('send confirm 1');
+                error_log(__('confirmmembershipnojournals.emails.subject'));
                 $pressNames = implode(', ', $memberPresses);
                 $mailable = new Mailable();
                 $mailable
-                    ->from(Config::getVar('email', 'default_envelope_sender'))
+                    ->from($senderMail)
                     ->body(__('confirmmembership.emails.body', [
                         'fullname' => $fullName,
                         'press' => $pressNames
@@ -96,12 +100,11 @@ class ConfirmMembershipTask extends ScheduledTask {
                 $mailable = new Mailable();
                 error_log('send confirm 1');
                 $mailable
-                    ->from(Config::getVar('email', 'default_envelope_sender'))
+                    ->from($senderMail)
                     ->body(__('confirmmembershipnojournals.emails.body', [
                         'fullname' => $fullName,
                     ], null))
                     ->subject(__('confirmmembershipnojournals.emails.subject'));
-                $emailTemplate =  Repo::emailTemplate()->getByKey(CONTEXT_SITE, 'COMFIRMMEMBERSHIP_NO_JOURNALS_MEMBERSHIP');//new EmailTemplate('COMFIRMMEMBERSHIP_NO_JOURNALS_MEMBERSHIP', 'en_US');
             }
 
 
@@ -130,7 +133,12 @@ class ConfirmMembershipTask extends ScheduledTask {
             $user->deleteDate(ConfirmMembershipPlugin::SETTING_CAN_NOT_DELETE);
         }
     }
-    private function mergeUsers($userDao, $pressDao, $roleIds, $user, $mergesUserId) {
+    private function mergeUsers($userDao, $pressDao, $roleIds, $user, $mergesUserId, $jobsAutoDeleteAge) {
+
+        if ($this->userHasReviews($user->getId(), $jobsAutoDeleteAge) || $this->userHasSubmission($user->getId(), $jobsAutoDeleteAge) ) {
+            $this->userCantBeDeleted($user, $userDao);
+            return;
+        }
 
         $presses = $pressDao->getAll();
         // Find the name(s) of the journals the user is signed up for and check roles and subscriptions
@@ -143,20 +151,22 @@ class ConfirmMembershipTask extends ScheduledTask {
             }
         }
         $this->userAction = new Repository($userDao);
-//        $userAction = $this->userAction;
-        $this->userAction->mergeUsers($user->getId(), $mergesUserId);
+
+        $this->userAction->mergeUsers($user->getId(), $mergesUserId, $jobsAutoDeleteAge);
     }
     private function userCantBeDeleted (&$user, $userDao) {
         DB::insert("insert into user_settings(user_id, locale, setting_name, setting_value ) values(?,'en', ?,?) ",[$user->getId(), 'membershipcannotdelete', true ]);
         return true;
     }
-    private function userHasReviews($userId) {
-        $checkUserSubmissions =DB::select('SELECT count(*)  AS row_count  FROM review_assignments  where reviewer_id = ? ', [$userId]); //DB::table('stage_assignments')->where('user_id', $userId);
+    private function userHasReviews($userId, $jobsAutoDeleteAge) {
+        $reviewersignmentDao = DAORegistry::getDAO('ReviewAssignmentDAO'); /** @var stageAssignmentDao StageAssignmentDAO */
+        $checkUserSubmissions = $reviewersignmentDao->retrieve("SELECT count(*)  AS row_count  FROM review_assignments  where reviewer_id = ? and last_modified > DATE(?) - interval ' $jobsAutoDeleteAge days' ", [$userId, Core::getCurrentDate()]);
         $row = $checkUserSubmissions->current();
         return $row ? (boolean) $row->row_count : false;
     }
-    private function userHasSubmission($userId) {
-        $checkUserSubmissions = DB::select("select count(*)  AS row_count from stage_assignments where user_id = ?", [$userId]); //DB::table('stage_assignments')->where('user_id', $userId);
+    private function userHasSubmission($userId, $jobsAutoDeleteAge) {
+        $stageAssignmentDao = DAORegistry::getDAO('StageAssignmentDAO'); /** @var stageAssignmentDao StageAssignmentDAO */
+        $checkUserSubmissions = $stageAssignmentDao->retrieve("select count(*) AS row_count  from stage_assignments join submissions on stage_assignments.submission_id =submissions.submission_id where last_modified  >  DATE(?) - interval ' $jobsAutoDeleteAge days' and user_id = ?", [Core::getCurrentDate(), $userId]);
         $current = $checkUserSubmissions->current();
         $row = $current;
         return $row ? (boolean) $row->row_count : false;
